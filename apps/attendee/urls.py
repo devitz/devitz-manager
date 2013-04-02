c2827d614ecf99c2a7e86e1de1dc6dcf4c435740
Herson Leite <hersonls@gmail.com>
2013-04-02 08:42:32 -0300

    Update urls.py
    Removed to open new accounts creation area

--- apps/attendee/urls.py
+++ apps/attendee/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 from .views import confirm_account_view
 
 urlpatterns = patterns('',
-    url(r'^confirm-account/(?P<key>.*)/$', confirm_account_view, name='attendee_attendee_confirm')
+    # Removed to open new accounts creation area
+    # url(r'^confirm-account/(?P<key>.*)/$', confirm_account_view, name='attendee_attendee_confirm')
 )
