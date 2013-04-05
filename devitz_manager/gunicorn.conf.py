a1e7d583b7253667a4eba37bd2f8488f581d56d7
Hersonls <hersonls@gmail.com>
2013-04-05 15:54:06 -0300

    Changed gunicorn settings to new settings place

--- devitz_manager/gunicorn.conf.py
+++ devitz_manager/gunicorn.conf.py
@@ -3,15 +3,13 @@ import sys
 import os
 import multiprocessing
 
-from django.conf import settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devitz_manager.settings.prod")
 
 try:
-    import settings
+    from django.conf import settings
 except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devitz_manager.settings.prod")
 
 bind = '127.0.0.1:9000'
 workers = multiprocessing.cpu_count()
