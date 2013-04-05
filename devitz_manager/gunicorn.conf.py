80aa1e5d93b5b7c3b2ff9610c19cbdb4a7c93cb2
Hersonls <hersonls@gmail.com>
2013-04-05 15:33:00 -0300

    Changed django settings

--- devitz_manager/gunicorn.conf.py
+++ devitz_manager/gunicorn.conf.py
@@ -11,6 +11,8 @@ except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
     sys.exit(1)
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devitz_manager.settings.prod")
+
 bind = '127.0.0.1:9000'
 workers = multiprocessing.cpu_count()
 worker_connections = 2048
